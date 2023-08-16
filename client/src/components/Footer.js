1cebe00e8fb0b0758c8809752f327ac21cfac33c
IsaiasD18 <133791406+IsaiasD18@users.noreply.github.com>
2023-08-16 02:32:58 -0400

    created the footer and header for the app

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,37 @@
-function Footer(propsObj) {
-    const date = new Date();
-    const year = date.getFullYear();
-  
-    return (
-      <footer className="row justify-between">
-        <p>&copy; {year}</p>
-        <p>Dev'd and Designed by Isaias F</p>
-      </footer>
-    )
-  }
-  
-  export default Footer;
+import React from 'react';
+import { FaGithub, FaLinkedin, FaStackOverflow } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <footer className="bg-gradient-to-r from-green-800 to-green-400 py-6 fixed bottom-0 w-full">
+      <div className="flex justify-center">
+        <a
+          href="https://github.com/IsaiasD18"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-300 mx-2 sm:mx-4"
+        >
+          <FaGithub size={24} />
+        </a>
+        <a
+          href="https://linkedin.com/in/your-linkedin-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-300 mx-2 sm:mx-4"
+        >
+          <FaLinkedin size={24} />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/your-stackoverflow-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-300 mx-2 sm:mx-4"
+        >
+          <FaStackOverflow size={24} />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
